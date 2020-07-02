eaeead9d4450cdfa919e4495e6ec53af2dabe07d
tshak <github@tshak.net>
2020-07-02 12:49:32 +0200

    Bump riser-server version to 0.0.26

--- pkg/infra/riserDeployment.go
+++ pkg/infra/riserDeployment.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	DefaultEnvironmentName = "demo"
-	DefaultServerImage     = "riserplatform/riser-server:0.0.25"
+	DefaultServerImage     = "riserplatform/riser-server:0.0.26"
 	DefaultControllerImage = "riserplatform/riser-controller:0.0.8"
 )
 
