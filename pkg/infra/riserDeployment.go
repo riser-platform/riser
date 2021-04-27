2b2464708f2592120b8435608fffca5ac1c12ed5
tshak <github@tshak.net>
2021-04-27 14:54:01 +0200

    Bump default riser server to v0.0.46

--- pkg/infra/riserDeployment.go
+++ pkg/infra/riserDeployment.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	DefaultEnvironmentName = "demo"
-	DefaultServerImage     = "ghcr.io/riser-platform/riser-server:v0.0.45"
+	DefaultServerImage     = "ghcr.io/riser-platform/riser-server:v0.0.46"
 	DefaultControllerImage = "ghcr.io/riser-platform/riser-controller:v0.0.17"
 )
 
